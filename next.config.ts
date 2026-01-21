396ad3ff100e848d49666b932e5c7fd603dc7d0c
sorecauadrian <sorecauadrian@gmail.com>
2026-01-21 14:31:10 +0200

    solved deployment errors regarding photos folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingExcludes: {
+    '*': [
+      'public/photos/**/*',
+      './public/photos/**/*',
+    ],
+  },
 };
 
 export default nextConfig;
